0fc1389dfafd1f6d07f3010ba04f4dca9e2bcfe5
GRAYSON_LAPTOP\Grayson <grayson.ingram@att.net>
2018-08-03 12:32:37 -0500

    Some changes

--- BestBuyCorporate/Program.cs
+++ BestBuyCorporate/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Text;
 namespace BestBuyCorporate
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                .AddJsonFile("appsettings.debug.json")
+#else
+                .AddJsonFile("appsettings.release.json")
+#endif
+                .Build();
+            string connStr = configBuilder.GetConnectionString("DefaultConnection");
+
+            
+
+            
         }
     }
 }
