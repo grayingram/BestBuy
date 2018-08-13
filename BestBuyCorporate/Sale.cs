4eb2946201ffe98c3ad125afeddcf92bc1d3f213
Unknown <grayson.ingram@att.net>
2018-08-13 08:42:31 -0500

    Sale and other changes
    Added a sale class and made the lists of categories and products something the reader contains

---
+++ BestBuyCorporate/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuyCorporate
+{
+    class Sale
+    {
+        public int SaleId { get; private set; }
+        public int ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public double Price { get; private set; }
+        public DateTime date { get; private set; }
+    }
+}
